b5d407618b26dec29ff777a45ab4beaa4b96780b
Abhisek-Ray99 <rickyrayabhisek99@gmail.com>
2020-12-26 22:05:16 +0530

    added yelp clone

---
+++ frontend/src/routes/RestaurantdetailPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RestaurantdetailPage = () => {
+    return (
+        <div>
+            RestaurantdetailPage
+        </div>
+    )
+}
+
+export default RestaurantdetailPage
